da6fe922acc522d16c0008c11baa5ebd0c2fc01a
Vladimir Chertilov <chertilov@reksoft.ru>
2021-07-29 20:39:37 +0300

    rx_homework/implemented LocalCatFactsGenerator functions and getFacts method in CatsViewModel

--- app/src/main/java/otus/homework/reactivecats/BaseViewModel.kt
+++ app/src/main/java/otus/homework/reactivecats/BaseViewModel.kt
@@ -1,6 +1,8 @@
 package otus.homework.reactivecats
 
 import androidx.lifecycle.ViewModel
+import io.reactivex.Observable
+import io.reactivex.Observer
 import io.reactivex.Single
 import io.reactivex.SingleObserver
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -17,31 +19,49 @@ open class BaseViewModel : ViewModel() {
         super.onCleared()
     }
 
-    protected fun <E> Single<E>.execute(onSuccess: (E) -> Unit) =
-        execute(onSuccess, onError = {})
-
     protected fun <E> Single<E>.execute(
         onSuccess: (E) -> Unit,
-        onError: (Throwable) -> Unit = { },
-        onSubscribe: (Disposable) -> Unit = {},
-        onFinished: () -> Unit = {}
+        onError: (Throwable) -> Unit = { }
     ) {
         observeOn(AndroidSchedulers.mainThread())
             .subscribeOn(Schedulers.io())
             .subscribe(object : SingleObserver<E> {
                 override fun onSuccess(t: E) {
                     onSuccess(t)
-                    onFinished()
                 }
 
                 override fun onSubscribe(d: Disposable) {
                     disposables.add(d)
-                    onSubscribe(d)
                 }
 
                 override fun onError(e: Throwable) {
                     onError(e)
-                    onFinished()
+                }
+            })
+    }
+
+    protected fun <E> Observable<E>.execute(onNext: (E) -> Unit) = execute(onNext, onError = {})
+
+    protected fun <E> Observable<E>.execute(
+        onNext: (E) -> Unit,
+        onError: (Throwable) -> Unit = { }
+    ) {
+        observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
+            .subscribe(object : Observer<E> {
+                override fun onComplete() {
+                }
+
+                override fun onSubscribe(d: Disposable) {
+                    disposables.add(d)
+                }
+
+                override fun onNext(t: E) {
+                    onNext(t)
+                }
+
+                override fun onError(e: Throwable) {
+                    onError(e)
                 }
             })
     }
--- app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
+++ app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
@@ -5,29 +5,40 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import io.reactivex.Observable
 import retrofit2.HttpException
+import java.util.concurrent.TimeUnit
 
 class CatsViewModel(
-    catsService: CatsService,
-    localCatFactsGenerator: LocalCatFactsGenerator,
+    private val catsService: CatsService,
+    private val localCatFactsGenerator: LocalCatFactsGenerator,
     context: Context
 ) : BaseViewModel() {
 
     private val _catsLiveData = MutableLiveData<Result>()
     val catsLiveData: LiveData<Result> = _catsLiveData
 
+    private val errorMessage = context.getString(R.string.default_error_text)
+
     init {
         catsService.getCatFact().execute(
             onSuccess = { _catsLiveData.value = Success(it) },
-            onError = { handleError(it, context) }
+            onError = ::handleError
         )
     }
 
-    fun getFacts() {}
+    fun getFacts() {
+        Observable.interval(2, TimeUnit.SECONDS)
+            .flatMapSingle {
+                catsService.getCatFact()
+                    .onErrorResumeNext(localCatFactsGenerator.generateCatFact())
+            }
+            .execute { _catsLiveData.value = Success(it) }
+    }
 
-    private fun handleError(e: Throwable, context: Context) {
+    private fun handleError(e: Throwable) {
         if (e is HttpException) _catsLiveData.value = ServerError
-        else e.message ?: context.getString(R.string.default_error_text)
+        else e.message ?: errorMessage
     }
 }
 
