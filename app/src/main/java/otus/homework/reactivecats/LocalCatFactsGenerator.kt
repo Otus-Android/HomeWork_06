0265bea9d7a84e0bc30f038dc37f59d8ee76b3a8
Юрий Польщиков <Polshchikov.Y.Va@sberbank.ru>
2021-08-23 23:37:39 +0300

    homework_06_rxjava

--- app/src/main/java/otus/homework/reactivecats/LocalCatFactsGenerator.kt
+++ app/src/main/java/otus/homework/reactivecats/LocalCatFactsGenerator.kt
@@ -1,8 +1,10 @@
 package otus.homework.reactivecats
 
 import android.content.Context
+import io.reactivex.BackpressureStrategy
 import io.reactivex.Flowable
 import io.reactivex.Single
+import java.util.concurrent.TimeUnit
 import kotlin.random.Random
 
 class LocalCatFactsGenerator(
@@ -14,8 +16,9 @@ class LocalCatFactsGenerator(
      * чтобы она возвращала Fact со случайной строкой  из массива строк R.array.local_cat_facts
      * обернутую в подходящий стрим(Flowable/Single/Observable и т.п)
      */
-    fun generateCatFact(): Single<Fact> {
-        return Single.never()
+    fun generateCatFact(): Single<List<Fact>> {
+        val randomFact = context.resources.getStringArray(R.array.local_cat_facts).random()
+        return Single.just(listOf(Fact(randomFact)))
     }
 
     /**
@@ -23,8 +26,15 @@ class LocalCatFactsGenerator(
      * чтобы она эмитила Fact со случайной строкой из массива строк R.array.local_cat_facts каждые 2000 миллисекунд.
      * Если вновь заэмиченный Fact совпадает с предыдущим - пропускаем элемент.
      */
-    fun generateCatFactPeriodically(): Flowable<Fact> {
-        val success = Fact(context.resources.getStringArray(R.array.local_cat_facts)[Random.nextInt(5)])
-        return Flowable.empty()
+    fun generateCatFactPeriodically(): Flowable<List<Fact>> {
+        return Flowable.interval(0, 2000, TimeUnit.MILLISECONDS)
+            .flatMap {
+                Flowable.create<List<Fact>>({ emitter ->
+                    val fact = Fact(context.resources.getStringArray(R.array.local_cat_facts)[Random.nextInt(5)])
+                    emitter.onNext(listOf(fact))
+                    emitter.onComplete()
+                }, BackpressureStrategy.BUFFER)
+            }
+            .distinctUntilChanged()
     }
 }
