c927b9db12b135ec8bcda1dfc55ef4b2e7b0bc8d
abzal <abz@mail.com>
2021-09-03 15:42:15 +0600

    done homework

--- app/src/main/java/otus/homework/reactivecats/CatsService.kt
+++ app/src/main/java/otus/homework/reactivecats/CatsService.kt
@@ -1,10 +1,11 @@
 package otus.homework.reactivecats
 
+import io.reactivex.Single
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface CatsService {
 
     @GET("dogs?number=1")
-    fun getCatFact(): Call<List<Fact>>
+    fun getCatFact(): Single<List<Fact>>
 }
