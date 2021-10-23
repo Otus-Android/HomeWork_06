2aa14f5840f72b903b486afe4db1fa3512c4584e
Alexander Totilas <totilas.aleks@gmail.com>
2021-10-23 19:45:13 +0300

    refactor: replace Retrofit callbacks by RX;

--- app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
+++ app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
@@ -5,40 +5,52 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import retrofit2.HttpException
 
 class CatsViewModel(
     catsService: CatsService,
     localCatFactsGenerator: LocalCatFactsGenerator,
     context: Context
 ) : ViewModel() {
-
+    
+    private val disposables = CompositeDisposable()
+    
     private val _catsLiveData = MutableLiveData<Result>()
     val catsLiveData: LiveData<Result> = _catsLiveData
 
     init {
-        catsService.getCatFact().enqueue(object : Callback<Fact> {
-            override fun onResponse(call: Call<Fact>, response: Response<Fact>) {
-                if (response.isSuccessful && response.body() != null) {
-                    _catsLiveData.value = Success(response.body()!!)
-                } else {
-                    _catsLiveData.value = Error(
-                        response.errorBody()?.string() ?: context.getString(
-                            R.string.default_error_text
-                        )
-                    )
+        val factDisposable = catsService.getCatFact()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { fact ->
+                    _catsLiveData.value = Success(fact)
+                },
+                { e ->
+                    _catsLiveData.value = handleError(e, context)
                 }
-            }
-
-            override fun onFailure(call: Call<Fact>, t: Throwable) {
-                _catsLiveData.value = ServerError
-            }
-        })
+            )
+        disposables.add(factDisposable)
+    }
+    
+    private fun handleError(e: Throwable, context: Context): Result {
+        return if (e is HttpException) {
+            Error(e.response()?.errorBody()?.string() ?: context.getString(R.string.default_error_text))
+        } else {
+            ServerError
+        }
     }
 
     fun getFacts() {}
+    
+    override fun onCleared() {
+        super.onCleared()
+        disposables.dispose()
+    }
+    
 }
 
 class CatsViewModelFactory(
--- app/src/main/java/otus/homework/reactivecats/DiContainer.kt
+++ app/src/main/java/otus/homework/reactivecats/DiContainer.kt
@@ -1,7 +1,10 @@
 package otus.homework.reactivecats
 
 import android.content.Context
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 class DiContainer {
@@ -10,9 +13,19 @@ class DiContainer {
         Retrofit.Builder()
             .baseUrl("https://cat-fact.herokuapp.com/facts/")
             .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .client(createHttpClient())
             .build()
     }
-
+    
+    private fun createHttpClient(): OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(
+            HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            }
+        )
+        .build()
+    
     val service by lazy { retrofit.create(CatsService::class.java) }
 
     fun localCatFactsGenerator(context: Context) = LocalCatFactsGenerator(context)
