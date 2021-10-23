07313d47d54024795ee0c9f61a2b4f41dcf84262
Alexander Totilas <totilas.aleks@gmail.com>
2021-10-23 19:59:28 +0300

    feature: random cat fact periodical generation method;

--- app/src/main/java/otus/homework/reactivecats/LocalCatFactsGenerator.kt
+++ app/src/main/java/otus/homework/reactivecats/LocalCatFactsGenerator.kt
@@ -3,6 +3,7 @@ package otus.homework.reactivecats
 import android.content.Context
 import io.reactivex.Flowable
 import io.reactivex.Single
+import java.util.concurrent.TimeUnit
 
 class LocalCatFactsGenerator(
     private val context: Context
@@ -21,11 +22,17 @@ class LocalCatFactsGenerator(
      * Если вновь заэмиченный Fact совпадает с предыдущим - пропускаем элемент.
      */
     fun generateCatFactPeriodically(): Flowable<Fact> {
-        val success = Fact(context.resources.getStringArray(R.array.local_cat_facts)[Random.nextInt(5)])
-        return Flowable.empty()
+        return Flowable.interval(CAT_FACT_GENERATOR_INTERVAL, TimeUnit.MILLISECONDS)
+            .map { getCatFacts().random() }
+            .distinctUntilChanged()
+            .map { text -> Fact(text) }
     }
     
     private fun getCatFacts(): Array<String> =
         context.resources.getStringArray(R.array.local_cat_facts)
+    
+    companion object {
+        private const val CAT_FACT_GENERATOR_INTERVAL = 2_000L
+    }
     
 }
