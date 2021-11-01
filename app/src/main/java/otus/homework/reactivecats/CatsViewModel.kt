c816bd803439d11019e7cd1c2f03579aaab6e753
Gavrilov V <viacheslav.gavrilov@domru.ru>
2021-11-01 11:00:40 +0300

    ReactiveCats

--- app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
+++ app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
@@ -5,22 +5,28 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 class CatsViewModel(
-    catsService: CatsService,
-    localCatFactsGenerator: LocalCatFactsGenerator,
+    private val catsService: CatsService,
+    private val localCatFactsGenerator: LocalCatFactsGenerator,
     context: Context
 ) : ViewModel() {
 
     private val _catsLiveData = MutableLiveData<Result>()
     val catsLiveData: LiveData<Result> = _catsLiveData
+    private var disposable: Disposable? = null
 
     init {
-        catsService.getCatFact().enqueue(object : Callback<Fact> {
-            override fun onResponse(call: Call<Fact>, response: Response<Fact>) {
+        disposable = catsService.getCatFact()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+            { response ->
                 if (response.isSuccessful && response.body() != null) {
                     _catsLiveData.value = Success(response.body()!!)
                 } else {
@@ -30,15 +36,25 @@ class CatsViewModel(
                         )
                     )
                 }
-            }
-
-            override fun onFailure(call: Call<Fact>, t: Throwable) {
+            },
+            {
                 _catsLiveData.value = ServerError
-            }
-        })
+            })
     }
 
-    fun getFacts() {}
+    fun getFacts(): Observable<Fact> {
+        return Observable.interval(2, TimeUnit.SECONDS)
+            .flatMap {
+                catsService.getCatFact()
+                    .map {it.body()}
+                    .onErrorResumeNext(localCatFactsGenerator.generateCatFact().toObservable())
+            }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposable?.dispose()
+    }
 }
 
 class CatsViewModelFactory(
