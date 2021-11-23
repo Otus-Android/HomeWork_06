d528aecfd5b2af6e6dee4ab568eb5fc3e9cd74a7
D_Mironov_29682 <d.mironov@rendez-vous.ru>
2021-11-23 19:46:42 +0300

    done task 5

--- app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
+++ app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
@@ -5,14 +5,18 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import io.reactivex.Flowable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
+
+private const val PERIOD: Long = 2000L
 
 class CatsViewModel(
-    catsService: CatsService,
-    localCatFactsGenerator: LocalCatFactsGenerator,
-    context: Context
+    private val catsService: CatsService,
+    private val localCatFactsGenerator: LocalCatFactsGenerator,
+    private val context: Context
 ) : ViewModel() {
 
     private val _catsLiveData = MutableLiveData<Result>()
@@ -20,6 +24,10 @@ class CatsViewModel(
     private val disposables = CompositeDisposable()
 
     init {
+        getFacts()
+    }
+
+    private fun getFact() {
         disposables.add(
             catsService.getCatFact()
                 .subscribeOn(Schedulers.io())
@@ -33,7 +41,22 @@ class CatsViewModel(
                 ))
     }
 
-    fun getFacts() {}
+    private fun getFacts() {
+        disposables.add(
+            Flowable.interval(PERIOD, TimeUnit.MILLISECONDS, Schedulers.io())
+                .flatMapSingle {
+                    catsService.getCatFact()
+                        .onErrorResumeNext(localCatFactsGenerator.generateCatFact())
+                }
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                    { _catsLiveData.value = Success(it) },
+                    {
+                        _catsLiveData.value =
+                            Error(it.message ?: context.getString(R.string.default_error_text))
+                    }
+                ))
+    }
 
     override fun onCleared() {
         super.onCleared()
