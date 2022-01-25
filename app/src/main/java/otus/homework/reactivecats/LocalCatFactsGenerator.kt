a0c33e23f3700fd2a074c9531e362803dde08407
Olga Ivanova <olga.124@list.ru>
2022-01-25 14:16:40 +0300

    task3 (generateCatFact)

--- app/src/main/java/otus/homework/reactivecats/LocalCatFactsGenerator.kt
+++ app/src/main/java/otus/homework/reactivecats/LocalCatFactsGenerator.kt
@@ -9,13 +9,15 @@ class LocalCatFactsGenerator(
     private val context: Context
 ) {
 
+    private val catFacts = context.resources.getStringArray(R.array.local_cat_facts)
+
     /**
      * Реализуйте функцию otus.homework.reactivecats.LocalCatFactsGenerator#generateCatFact так,
      * чтобы она возвращала Fact со случайной строкой  из массива строк R.array.local_cat_facts
      * обернутую в подходящий стрим(Flowable/Single/Observable и т.п)
      */
     fun generateCatFact(): Single<Fact> {
-        return Single.never()
+        return Single.just(Fact(catFacts.random()))
     }
 
     /**
