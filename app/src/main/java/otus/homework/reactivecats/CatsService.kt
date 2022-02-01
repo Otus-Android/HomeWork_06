e265022774493d1887626484e398745647b91092
Necris174 <plotnicovvyu@gmail.com>
2022-02-01 13:14:49 +0500

    HW06 Plotnikov

--- app/src/main/java/otus/homework/reactivecats/CatsService.kt
+++ app/src/main/java/otus/homework/reactivecats/CatsService.kt
@@ -1,10 +1,12 @@
 package otus.homework.reactivecats
 
-import retrofit2.Call
+
+import io.reactivex.Single
 import retrofit2.http.GET
+
 
 interface CatsService {
 
     @GET("random?animal_type=cat")
-    fun getCatFact(): Call<Fact>
+    fun getCatFact(): Single<Fact>
 }
