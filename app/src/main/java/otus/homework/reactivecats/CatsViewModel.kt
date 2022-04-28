6253214765809815916d376f5b781160237f5b9c
Sergey Kudashkin <sikuda@yandex.ru>
2022-04-28 12:43:02 +0300

    1. Call` на RX цепочку 2. Model с колбеков на RX

--- app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
+++ app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
@@ -5,6 +5,11 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import io.reactivex.Single
+import io.reactivex.SingleObserver
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -18,24 +23,48 @@ class CatsViewModel(
     private val _catsLiveData = MutableLiveData<Result>()
     val catsLiveData: LiveData<Result> = _catsLiveData
 
-    init {
-        catsService.getCatFact().enqueue(object : Callback<Fact> {
-            override fun onResponse(call: Call<Fact>, response: Response<Fact>) {
-                if (response.isSuccessful && response.body() != null) {
-                    _catsLiveData.value = Success(response.body()!!)
-                } else {
-                    _catsLiveData.value = Error(
-                        response.errorBody()?.string() ?: context.getString(
-                            R.string.default_error_text
-                        )
-                    )
-                }
-            }
+    val dispose = object: DisposableSingleObserver<Fact>() {
+        override fun onSuccess(response: Fact) {
+            _catsLiveData.value = Success(response)
+        }
 
-            override fun onFailure(call: Call<Fact>, t: Throwable) {
-                _catsLiveData.value = ServerError
-            }
-        })
+        override fun onError(e: Throwable) {
+            _catsLiveData.value = Error(e.message.toString())
+        }
+    }
+
+    init {
+
+        catsService.getCatFact()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(dispose)
+
+//        catsService.getCatFact().enqueue(object : Callback<Fact> {
+//            override fun onResponse(call: Call<Fact>, response: Response<Fact>) {
+//                if (response.isSuccessful && response.body() != null) {
+//                    _catsLiveData.value = Success(response.body()!!)
+//                } else {
+//                    _catsLiveData.value = Error(
+//                        response.errorBody()?.string() ?: context.getString(
+//                            R.string.default_error_text
+//                        )
+//                    )
+//                }
+//            }
+//
+//            override fun onFailure(call: Call<Fact>, t: Throwable) {
+//                _catsLiveData.value = ServerError
+//            }
+//        })
+
+
+
+    }
+
+    override fun onCleared() {
+        dispose.dispose()
+        super.onCleared()
     }
 
     fun getFacts() {}
