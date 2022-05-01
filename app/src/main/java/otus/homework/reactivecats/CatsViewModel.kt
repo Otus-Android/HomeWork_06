6057e76c41df1c7c6d34b525e96a3a008c21a7d3
Roman Belyanin <bel.r.s@yandex.ru>
2022-05-01 16:17:55 +0300

    1. Переведите сетевой запрос с `retrofit.Call` на RX цепочку.

--- app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
+++ app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
@@ -5,6 +5,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -18,24 +22,22 @@ class CatsViewModel(
     private val _catsLiveData = MutableLiveData<Result>()
     val catsLiveData: LiveData<Result> = _catsLiveData
 
-    init {
-        catsService.getCatFact().enqueue(object : Callback<Fact> {
-            override fun onResponse(call: Call<Fact>, response: Response<Fact>) {
-                if (response.isSuccessful && response.body() != null) {
-                    _catsLiveData.value = Success(response.body()!!)
-                } else {
-                    _catsLiveData.value = Error(
-                        response.errorBody()?.string() ?: context.getString(
-                            R.string.default_error_text
-                        )
-                    )
-                }
-            }
+    val disposable = CompositeDisposable()
 
-            override fun onFailure(call: Call<Fact>, t: Throwable) {
+    init {
+        disposable.add(catsService.getCatFact()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                _catsLiveData.value = Success(it)
+            },{
                 _catsLiveData.value = ServerError
-            }
-        })
+//                _catsLiveData.value = Error(
+//                    response.errorBody()?.string() ?: context.getString(
+//                        R.string.default_error_text
+//                    )
+//                )
+            }))
     }
 
     fun getFacts() {}
