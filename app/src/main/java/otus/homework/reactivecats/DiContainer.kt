a14403501287d56292491266d377df1cb149b80f
ilya <lex7555@yandex.ru>
2022-05-10 22:27:48 +0300

    Сделал задачи 1-5.

--- app/src/main/java/otus/homework/reactivecats/DiContainer.kt
+++ app/src/main/java/otus/homework/reactivecats/DiContainer.kt
@@ -1,8 +1,9 @@
 package otus.homework.reactivecats
 
 import android.content.Context
+import io.reactivex.disposables.CompositeDisposable
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 class DiContainer {
@@ -11,11 +12,13 @@ class DiContainer {
         Retrofit.Builder()
             .baseUrl("https://cat-fact.herokuapp.com/facts/")
             .addConverterFactory(GsonConverterFactory.create())
-            .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .build()
     }
 
-    val service by lazy { retrofit.create(CatsService::class.java) }
+    val service: CatsService by lazy { retrofit.create(CatsService::class.java) }
+
+    val subscriptions: CompositeDisposable = CompositeDisposable()
 
     fun localCatFactsGenerator(context: Context) = LocalCatFactsGenerator(context)
 }
