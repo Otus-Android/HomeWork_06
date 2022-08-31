3a79049258f7c9157de336e33c838c59402ed95b
yolis_storn <elisstormbreaker@gmail.com>
2022-08-31 17:51:38 +0300

    Изменена логика в `CatsViewModel.kt` с колбеков на RX.

--- app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
+++ app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
@@ -1,44 +1,56 @@
 package otus.homework.reactivecats
 
 import android.content.Context
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.OnLifecycleEvent
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import retrofit2.HttpException
 
 class CatsViewModel(
     catsService: CatsService,
     localCatFactsGenerator: LocalCatFactsGenerator,
     context: Context
-) : ViewModel() {
+) : ViewModel(), LifecycleObserver {
 
     private val _catsLiveData = MutableLiveData<Result>()
     val catsLiveData: LiveData<Result> = _catsLiveData
 
-    init {
-        catsService.getCatFact().enqueue(object : Callback<Fact> {
-            override fun onResponse(call: Call<Fact>, response: Response<Fact>) {
-                if (response.isSuccessful && response.body() != null) {
-                    _catsLiveData.value = Success(response.body()!!)
-                } else {
-                    _catsLiveData.value = Error(
-                        response.errorBody()?.string() ?: context.getString(
-                            R.string.default_error_text
-                        )
-                    )
-                }
-            }
+    private val disposables = CompositeDisposable()
 
-            override fun onFailure(call: Call<Fact>, t: Throwable) {
-                _catsLiveData.value = ServerError
-            }
-        })
+    init {
+        disposables.add(
+            catsService
+                .getCatFact()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    _catsLiveData.value = Success(it)
+                }, { ex ->
+                    when (ex) {
+                        is HttpException -> {
+                            _catsLiveData.value = Error(ex.message ?: context.getString(
+                                R.string.default_error_text
+                            ))
+                        }
+                        else -> _catsLiveData.value = ServerError
+                    }
+                })
+        )
     }
 
     fun getFacts() {}
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    private fun stopObserver() {
+        disposables.clear()
+    }
 }
 
 class CatsViewModelFactory(
