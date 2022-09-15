1dd8bc511be0a910efc74122154cce94d5beb0d1
D_Mironov_29682 <d.mironov@rendez-vous.ru>
2022-09-15 07:11:28 +0300

    changed return type

--- app/src/main/java/otus/homework/reactivecats/DiContainer.kt
+++ app/src/main/java/otus/homework/reactivecats/DiContainer.kt
@@ -3,12 +3,14 @@ package otus.homework.reactivecats
 import android.content.Context
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 
 class DiContainer {
 
     private val retrofit by lazy {
         Retrofit.Builder()
             .baseUrl("https://cat-fact.herokuapp.com/facts/")
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
