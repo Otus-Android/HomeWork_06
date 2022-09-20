f15f1a93bf33c438013ebc4ec78b5c5389e39c53
Тимур Фаршатов <farshatov@yandex.ru>
2022-09-20 19:16:26 +0500

    Task 1

--- app/src/main/java/otus/homework/reactivecats/CatsService.kt
+++ app/src/main/java/otus/homework/reactivecats/CatsService.kt
@@ -1,9 +1,13 @@
 package otus.homework.reactivecats
 
+import io.reactivex.Single
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface CatsService {
+
+    @GET("random?animal_type=cat")
+    fun getCatFactReactive(): Single<Fact>
 
     @GET("random?animal_type=cat")
     fun getCatFact(): Call<Fact>
