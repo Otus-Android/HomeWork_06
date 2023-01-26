497ad2e439fce1bd1ef7e2f4386f823570d1a90f
V <petrov.vladislav1992@gmail.com>
2023-01-26 14:13:01 +0600

    Done?

--- app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
+++ app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
@@ -5,40 +5,82 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 class CatsViewModel(
-    catsService: CatsService,
-    localCatFactsGenerator: LocalCatFactsGenerator,
+    private  val catsService: CatsService,
+    private  val localCatFactsGenerator: LocalCatFactsGenerator,
     context: Context
 ) : ViewModel() {
 
     private val _catsLiveData = MutableLiveData<Result>()
     val catsLiveData: LiveData<Result> = _catsLiveData
 
-    init {
-        catsService.getCatFact().enqueue(object : Callback<Fact> {
-            override fun onResponse(call: Call<Fact>, response: Response<Fact>) {
-                if (response.isSuccessful && response.body() != null) {
-                    _catsLiveData.value = Success(response.body()!!)
-                } else {
-                    _catsLiveData.value = Error(
-                        response.errorBody()?.string() ?: context.getString(
-                            R.string.default_error_text
-                        )
-                    )
-                }
-            }
+    private var factSubscription: Disposable? = null
+    private var factPeriodicallySubscription: Disposable? = null
 
-            override fun onFailure(call: Call<Fact>, t: Throwable) {
-                _catsLiveData.value = ServerError
-            }
-        })
+    init {
+        factSubscription = catsService.getCatFact()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                {
+                    _catsLiveData.value = Success(it)
+                }, {
+                    _catsLiveData.value = Error(it.message ?: "Unknown error")
+                }
+            )
+
+        /*local periodically test*/
+
+        //factSubscription = localCatFactsGenerator.generateCatFactPeriodically().subscribe(
+        //    {
+        //        _catsLiveData.value = Success(it)
+        //   }, {
+        //       _catsLiveData.value = Error(it.message ?: "Unknown error")
+        //   }
+        //)
+
+        /*local periodically test*/
+
+        /*network periodically test*/
+        getFacts()
+        /*network periodically test*/
+
     }
 
-    fun getFacts() {}
+    private fun getFacts() {
+
+        factPeriodicallySubscription = Observable
+            .interval(2, TimeUnit.SECONDS).flatMap {
+                return@flatMap catsService.getCatFact().toObservable().onErrorResumeNext { _: Throwable ->
+                    localCatFactsGenerator.generateCatFact().toObservable()
+                }
+            }
+        //.debounce(2000, TimeUnit.MILLISECONDS)
+        .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(
+            {
+               _catsLiveData.value = Success(it)
+            }, {
+                _catsLiveData.value = Error(it.message ?: "Unknown error")
+            }
+        )
+
+    }
+
+
+    override fun onCleared() {
+        super.onCleared()
+        factSubscription?.dispose()
+        factPeriodicallySubscription?.dispose()
+    }
+
 }
 
 class CatsViewModelFactory(
--- app/src/main/java/otus/homework/reactivecats/LocalCatFactsGenerator.kt
+++ app/src/main/java/otus/homework/reactivecats/LocalCatFactsGenerator.kt
@@ -2,12 +2,22 @@ package otus.homework.reactivecats
 
 import android.content.Context
 import io.reactivex.Flowable
+import io.reactivex.Observable
 import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import org.reactivestreams.Publisher
+import java.util.concurrent.TimeUnit
 import kotlin.random.Random
 
 class LocalCatFactsGenerator(
     private val context: Context
 ) {
+
+    private fun randomFact(): Fact {
+        val localFactsArray = context.resources.getStringArray(R.array.local_cat_facts)
+        return Fact(localFactsArray.random())
+    }
 
     /**
      * Реализуйте функцию otus.homework.reactivecats.LocalCatFactsGenerator#generateCatFact так,
@@ -15,7 +25,9 @@ class LocalCatFactsGenerator(
      * обернутую в подходящий стрим(Flowable/Single/Observable и т.п)
      */
     fun generateCatFact(): Single<Fact> {
-        return Single.never()
+        return Single.fromCallable {
+            return@fromCallable randomFact()
+        }
     }
 
     /**
@@ -24,7 +36,14 @@ class LocalCatFactsGenerator(
      * Если вновь заэмиченный Fact совпадает с предыдущим - пропускаем элемент.
      */
     fun generateCatFactPeriodically(): Flowable<Fact> {
-        val success = Fact(context.resources.getStringArray(R.array.local_cat_facts)[Random.nextInt(5)])
-        return Flowable.empty()
+        return Flowable.interval(2, TimeUnit.SECONDS).flatMap<Fact?> {
+            return@flatMap Publisher {
+                it.onNext(randomFact())
+                it.onComplete()
+            }
+        }
+            .subscribeOn(Schedulers.io())
+            .distinctUntilChanged()
+            .observeOn(AndroidSchedulers.mainThread())
     }
 }
