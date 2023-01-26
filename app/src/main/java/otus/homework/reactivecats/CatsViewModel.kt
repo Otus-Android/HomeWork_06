46b795d0f6bae60620088f8b6ce7854de4ddc4f3
Kamil Tayupov <k.i.tayupov@mail.ru>
2023-01-26 21:03:20 +0300

    Перевел сетевой запрос и view model на RX

--- app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
+++ app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
@@ -5,9 +5,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.disposables.Disposables
+import io.reactivex.schedulers.Schedulers
 
 class CatsViewModel(
     catsService: CatsService,
@@ -18,27 +19,27 @@ class CatsViewModel(
     private val _catsLiveData = MutableLiveData<Result>()
     val catsLiveData: LiveData<Result> = _catsLiveData
 
-    init {
-        catsService.getCatFact().enqueue(object : Callback<Fact> {
-            override fun onResponse(call: Call<Fact>, response: Response<Fact>) {
-                if (response.isSuccessful && response.body() != null) {
-                    _catsLiveData.value = Success(response.body()!!)
-                } else {
-                    _catsLiveData.value = Error(
-                        response.errorBody()?.string() ?: context.getString(
-                            R.string.default_error_text
-                        )
-                    )
-                }
-            }
+    private var subscription: Disposable = Disposables.empty()
 
-            override fun onFailure(call: Call<Fact>, t: Throwable) {
-                _catsLiveData.value = ServerError
-            }
-        })
+    init {
+        subscription = catsService.getCatFact()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { _catsLiveData.value = Success(it) },
+                {
+                    val message = it.message ?: context.getString(R.string.default_error_text)
+                    _catsLiveData.value = Error(message)
+                }
+            )
     }
 
     fun getFacts() {}
+
+    override fun onCleared() {
+        super.onCleared()
+        subscription.dispose()
+    }
 }
 
 class CatsViewModelFactory(
