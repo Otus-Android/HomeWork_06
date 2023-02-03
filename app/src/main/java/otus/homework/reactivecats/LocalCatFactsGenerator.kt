f210a4f5e80ca9d28fa74a0207f37fe56c412d9a
Pavel Iohin <pavel@iohin.ru>
2023-02-03 22:04:28 +0300

    implemented generateCatFactPeriodically

--- app/src/main/java/otus/homework/reactivecats/LocalCatFactsGenerator.kt
+++ app/src/main/java/otus/homework/reactivecats/LocalCatFactsGenerator.kt
@@ -1,8 +1,9 @@
 package otus.homework.reactivecats
 
 import android.content.Context
-import io.reactivex.Flowable
+import io.reactivex.Observable
 import io.reactivex.Single
+import io.reactivex.subjects.BehaviorSubject
 import kotlin.random.Random
 
 class LocalCatFactsGenerator(
@@ -25,8 +26,13 @@ class LocalCatFactsGenerator(
      * чтобы она эмитила Fact со случайной строкой из массива строк R.array.local_cat_facts каждые 2000 миллисекунд.
      * Если вновь заэмиченный Fact совпадает с предыдущим - пропускаем элемент.
      */
-    fun generateCatFactPeriodically(): Flowable<Fact> {
-        val success = Fact(context.resources.getStringArray(R.array.local_cat_facts)[Random.nextInt(5)])
-        return Flowable.empty()
+    fun generateCatFactPeriodically(): Observable<Fact> {
+        val facts = context.resources.getStringArray(R.array.local_cat_facts)
+        return BehaviorSubject.generate<Fact?> {
+            val fact = facts[Random.nextInt(facts.size)]
+            it.onNext(Fact(fact))
+            Thread.sleep(2000)
+        }
+            .distinctUntilChanged()
     }
 }
