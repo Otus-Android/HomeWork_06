335b804a74aca47caeb8199baa69c6daaa536a84
Pavel Iohin <pavel@iohin.ru>
2023-02-03 22:29:43 +0300

    implemented getFacts

--- app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
+++ app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
@@ -5,18 +5,17 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 import java.net.SocketTimeoutException
+import java.util.concurrent.TimeUnit
 
 class CatsViewModel(
-    catsService: CatsService,
-    localCatFactsGenerator: LocalCatFactsGenerator,
-    context: Context
+    private val catsService: CatsService,
+    private val localCatFactsGenerator: LocalCatFactsGenerator,
+    private val context: Context
 ) : ViewModel() {
 
     private val _catsLiveData = MutableLiveData<Result>()
@@ -42,7 +41,25 @@ class CatsViewModel(
             })
     }
 
-    fun getFacts() {}
+    fun getFacts() {
+        factDisposable = Observable.interval(0, 2, TimeUnit.SECONDS)
+            .flatMapSingle { catsService.getCatFact() }
+            .subscribeOn(Schedulers.io())
+            .onErrorResumeNext(localCatFactsGenerator.generateCatFact().toObservable())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                _catsLiveData.value = Success(it)
+            }, {
+                when (it) {
+                    is SocketTimeoutException -> _catsLiveData.value = ServerError
+                    else -> _catsLiveData.value = Error(
+                        it.message ?: context.getString(
+                            R.string.default_error_text
+                        )
+                    )
+                }
+            })
+    }
 
     override fun onCleared() {
         super.onCleared()
