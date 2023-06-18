9b6934cd3b6d71b8b3fb3767d9fe67811b7a6765
isakovich <polinasmogi@gmail.com>
2023-06-18 23:10:36 +0300

    выполнила задания 1-5

--- app/src/main/java/otus/homework/reactivecats/CatsRepository.kt
+++ app/src/main/java/otus/homework/reactivecats/CatsRepository.kt
@@ -1,11 +1,7 @@
 package otus.homework.reactivecats
 
 import io.reactivex.Single
-import retrofit2.http.GET
 
-interface CatsService {
-
-    @GET("random?animal_type=cat")
+interface CatsRepository {
     fun getCatFact(): Single<Fact>
-
 }
--- app/src/main/java/otus/homework/reactivecats/MainActivity.kt
+++ app/src/main/java/otus/homework/reactivecats/MainActivity.kt
@@ -4,18 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.viewModels
-import androidx.lifecycle.lifecycleScope
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
     private val diContainer = DiContainer()
     private val catsViewModel by viewModels<CatsViewModel> {
-        CatsViewModelFactory(
-            diContainer.service,
-            diContainer.localCatFactsGenerator(applicationContext),
-            applicationContext
-        )
+        CatsViewModelFactory(diContainer.repository(applicationContext))
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,7 +20,11 @@ class MainActivity : AppCompatActivity() {
         catsViewModel.catsLiveData.observe(this) { result ->
             when (result) {
                 is Success -> view.populate(result.fact)
-                is Error -> Toast.makeText(this, result.message, Toast.LENGTH_LONG).show()
+                is Error -> Toast.makeText(
+                    this,
+                    result.message ?: getString(R.string.default_error_text),
+                    Toast.LENGTH_LONG
+                ).show()
                 ServerError -> Snackbar.make(view, "Network error", 1000).show()
             }
         }
