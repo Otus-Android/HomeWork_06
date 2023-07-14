bfc44eb980c70fb7eed33ad5ffbecab052de2c45
Dmitrii Lapenko <lapenko-da@zvuk.com>
2023-07-14 20:07:16 +0300

    part1. replace callback to rx

--- app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
+++ app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
@@ -5,8 +5,11 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import retrofit2.Call
-import retrofit2.Callback
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import retrofit2.Response
 
 class CatsViewModel(
@@ -15,30 +18,59 @@ class CatsViewModel(
     context: Context
 ) : ViewModel() {
 
+    private val compositeDisposable = CompositeDisposable()
+
     private val _catsLiveData = MutableLiveData<Result>()
     val catsLiveData: LiveData<Result> = _catsLiveData
 
     init {
-        catsService.getCatFact().enqueue(object : Callback<Fact> {
-            override fun onResponse(call: Call<Fact>, response: Response<Fact>) {
-                if (response.isSuccessful && response.body() != null) {
-                    _catsLiveData.value = Success(response.body()!!)
-                } else {
-                    _catsLiveData.value = Error(
-                        response.errorBody()?.string() ?: context.getString(
-                            R.string.default_error_text
+        compositeDisposable.add(
+            catsService.getCatFact()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                    onSuccess = { fact ->
+                        _catsLiveData.value = Success(fact)
+                    },
+                    onError = { error ->
+                        _catsLiveData.value = Error(
+                            error ?: context.getString(R.string.default_error_text)
                         )
-                    )
-                }
-            }
+                    },
+                    onFailure = {
+                        _catsLiveData.value = ServerError
+                    }
+                )
+        )
+    }
 
-            override fun onFailure(call: Call<Fact>, t: Throwable) {
-                _catsLiveData.value = ServerError
-            }
-        })
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.dispose()
     }
 
     fun getFacts() {}
+
+    private fun <T, R : Response<T>> Single<R>.subscribe(
+        onSuccess: (T) -> Unit,
+        onError: (String?) -> Unit,
+        onFailure: (Throwable) -> Unit
+    ): Disposable {
+        return subscribe(
+            { response ->
+                val responseBody = response.body()
+
+                if (response.isSuccessful && responseBody != null) {
+                    onSuccess(responseBody)
+                } else {
+                    onError(response.errorBody()?.string())
+                }
+            },
+            { throwable ->
+                onFailure(throwable)
+            }
+        )
+    }
 }
 
 class CatsViewModelFactory(
