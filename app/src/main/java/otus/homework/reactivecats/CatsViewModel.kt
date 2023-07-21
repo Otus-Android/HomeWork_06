0b17db3afa426de2d9666f6565c842e624bd815d
ivmakar <ivan.r.makarov@yandex.ru>
2023-07-21 11:38:33 +0300

    ADD -- Реализация функции getFacts()

--- app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
+++ app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
@@ -5,13 +5,15 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 class CatsViewModel(
-    catsService: CatsService,
-    localCatFactsGenerator: LocalCatFactsGenerator,
+    private val catsService: CatsService,
+    private val localCatFactsGenerator: LocalCatFactsGenerator,
     context: Context
 ) : ViewModel() {
 
@@ -22,7 +24,7 @@ class CatsViewModel(
 
     init {
         compositeDisposable.add(
-            catsService.getCatFact()
+            getFacts()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe ({
@@ -33,7 +35,16 @@ class CatsViewModel(
         )
     }
 
-    fun getFacts() {}
+    fun getFacts(): Observable<Fact> {
+        return Observable.timer(2, TimeUnit.SECONDS)
+            .repeat()
+            .flatMap {
+                catsService.getCatFact()
+                    .toObservable()
+                    .onErrorResumeNext(localCatFactsGenerator.generateCatFact().toObservable())
+            }
+
+    }
 
     override fun onCleared() {
         super.onCleared()
