9443901f0f4e54be0c33939d900cdb5a87bbb6ba
Maksim Evtushenko <meevtushenko@sberbank.ru>
2023-10-26 21:53:32 +0300

    Evtushenko Maksim RxJava task 1-2
    1) Подключил RxJava2CallAdapterFactory, для использования типов RxJava2. Поменял возвращаемый тип функции на Single, так как у нас одиночный запрос
    2) Перевел CatsViewModel запрос на RX цепочку

--- app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
+++ app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
@@ -5,40 +5,44 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 
 class CatsViewModel(
-    catsService: CatsService,
-    localCatFactsGenerator: LocalCatFactsGenerator,
-    context: Context
+    private val catsService: CatsService,
+    private val localCatFactsGenerator: LocalCatFactsGenerator,
+    private val context: Context
 ) : ViewModel() {
 
     private val _catsLiveData = MutableLiveData<Result>()
     val catsLiveData: LiveData<Result> = _catsLiveData
 
+    private lateinit var disposable: Disposable
+
     init {
-        catsService.getCatFact().enqueue(object : Callback<Fact> {
-            override fun onResponse(call: Call<Fact>, response: Response<Fact>) {
-                if (response.isSuccessful && response.body() != null) {
-                    _catsLiveData.value = Success(response.body()!!)
-                } else {
+        getFacts()
+    }
+
+    private fun getFacts() {
+        disposable = catsService.getCatFact()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { fact: Fact -> _catsLiveData.value = Success(fact) },
+                { throwable: Throwable ->
                     _catsLiveData.value = Error(
-                        response.errorBody()?.string() ?: context.getString(
+                        throwable.message ?: context.getString(
                             R.string.default_error_text
                         )
                     )
-                }
-            }
-
-            override fun onFailure(call: Call<Fact>, t: Throwable) {
-                _catsLiveData.value = ServerError
-            }
-        })
+                })
     }
 
-    fun getFacts() {}
+    override fun onCleared() {
+        super.onCleared()
+        disposable.dispose()
+    }
 }
 
 class CatsViewModelFactory(
