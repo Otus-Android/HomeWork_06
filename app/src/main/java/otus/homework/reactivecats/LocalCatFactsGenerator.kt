7d290c780e056c7ef618197df05984abfbc61068
jomborus <jomporus@gmail.com>
2023-12-07 00:07:21 +0300

    Do rx homework

--- app/src/main/java/otus/homework/reactivecats/LocalCatFactsGenerator.kt
+++ app/src/main/java/otus/homework/reactivecats/LocalCatFactsGenerator.kt
@@ -3,6 +3,8 @@ package otus.homework.reactivecats
 import android.content.Context
 import io.reactivex.Flowable
 import io.reactivex.Single
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 import kotlin.random.Random
 
 class LocalCatFactsGenerator(
@@ -15,7 +17,8 @@ class LocalCatFactsGenerator(
      * обернутую в подходящий стрим(Flowable/Single/Observable и т.п)
      */
     fun generateCatFact(): Single<Fact> {
-        return Single.never()
+        val fact = Fact(context.resources.getStringArray(R.array.local_cat_facts)[Random.nextInt(5)])
+        return Single.just(fact)
     }
 
     /**
@@ -25,6 +28,13 @@ class LocalCatFactsGenerator(
      */
     fun generateCatFactPeriodically(): Flowable<Fact> {
         val success = Fact(context.resources.getStringArray(R.array.local_cat_facts)[Random.nextInt(5)])
-        return Flowable.empty()
+        return Flowable
+            .interval(2000, TimeUnit.MILLISECONDS)
+            .flatMap { Flowable.fromCallable {
+                Fact(context.resources.getStringArray(R.array.local_cat_facts)[Random.nextInt(5)])
+            }
+                .subscribeOn(Schedulers.io())
+            }
+            .distinctUntilChanged()
     }
 }
