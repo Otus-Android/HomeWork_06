8aad3816d73601827c0dc193b3d48cbb3b82bf61
Alexandryan <artur@helix.am>
2024-01-15 14:29:11 +0400

    1. Переведите сетевой запрос с `retrofit.Call` на RX цепочку. Для этого подключите Retrofit адаптер,    поменяйте возвращаемые типы функций
    2. Поменяйте логику в `CatsViewModel.kt` с колбеков на RX. Логику обработки успеха/ошибки из
       коллбека необходимо перенести в терминальные коллбеки RX цепочки. Не забудьте очистить подписки
       когда `ViewModel` уничтожается
    
    3. Реализуйте функцию `otus.homework.reactivecats.LocalCatFactsGenerator#generateCatFact`, так,
       чтобы она возвращала `Fact` со случайной строкой из массива строк `R.array.local_cat_facts`
       обернутую в подходящий стрим(`Flowable`/`Single`/`Observable` и т.п)
    
    4. Реализуйте
       функцию `otus.homework.reactivecats.LocalCatFactsGenerator#generateCatFactPeriodically` так,
       чтобы она эмитила `Fact` со случайной строкой из массива строк `R.array.local_cat_facts` каждые
       2000 миллисекунд. Если вновь заэмиченный Fact совпадает с предыдущим - пропускаем элемент.
    
    5. Реализуйте функцию `otus.homework.reactivecats.CatsViewModel#getFacts` следующим образом:  каждые
       2 секунды идем в сеть за новым фактом, если сетевой запрос завершился неуспешно, то в качестве
       фоллбека идем за фактом в уже
       реализованный `otus.homework.reactivecats.LocalCatFactsGenerator#generateCatFact`.

--- app/src/main/java/otus/homework/reactivecats/LocalCatFactsGenerator.kt
+++ app/src/main/java/otus/homework/reactivecats/LocalCatFactsGenerator.kt
@@ -1,8 +1,16 @@
 package otus.homework.reactivecats
 
 import android.content.Context
+import android.os.SystemClock.sleep
+import android.util.Log
 import io.reactivex.Flowable
 import io.reactivex.Single
+import io.reactivex.SingleOnSubscribe
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+import java.util.concurrent.Executors.newSingleThreadExecutor
+import java.util.concurrent.TimeUnit
 import kotlin.random.Random
 
 class LocalCatFactsGenerator(
@@ -15,7 +23,8 @@ class LocalCatFactsGenerator(
      * обернутую в подходящий стрим(Flowable/Single/Observable и т.п)
      */
     fun generateCatFact(): Single<Fact> {
-        return Single.never()
+        val success = Fact(context.resources.getStringArray(R.array.local_cat_facts)[Random.nextInt(5)])
+        return Single.create { it.onSuccess(success) }
     }
 
     /**
@@ -24,7 +33,8 @@ class LocalCatFactsGenerator(
      * Если вновь заэмиченный Fact совпадает с предыдущим - пропускаем элемент.
      */
     fun generateCatFactPeriodically(): Flowable<Fact> {
-        val success = Fact(context.resources.getStringArray(R.array.local_cat_facts)[Random.nextInt(5)])
-        return Flowable.empty()
+        return Flowable.interval(1, TimeUnit.SECONDS).map {
+            Fact(context.resources.getStringArray(R.array.local_cat_facts)[Random.nextInt(5)])
+        }
     }
 }
