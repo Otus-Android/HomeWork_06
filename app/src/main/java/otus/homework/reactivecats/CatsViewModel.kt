0bef9d36a66aba84bd3d4404136a6168f7141c1c
stanislavrepinskas <repinskas@gmail.com>
2024-02-10 16:01:04 +0300

    complete

--- app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
+++ app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
@@ -5,40 +5,80 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import io.reactivex.Flowable
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
+
 
 class CatsViewModel(
-    catsService: CatsService,
-    localCatFactsGenerator: LocalCatFactsGenerator,
-    context: Context
+    private val catsService: CatsService,
+    private val localCatFactsGenerator: LocalCatFactsGenerator,
+    private val context: Context
 ) : ViewModel() {
 
     private val _catsLiveData = MutableLiveData<Result>()
     val catsLiveData: LiveData<Result> = _catsLiveData
 
+    private var compositeDisposable = CompositeDisposable()
+
     init {
-        catsService.getCatFact().enqueue(object : Callback<Fact> {
-            override fun onResponse(call: Call<Fact>, response: Response<Fact>) {
-                if (response.isSuccessful && response.body() != null) {
-                    _catsLiveData.value = Success(response.body()!!)
-                } else {
+        getFacts()
+    }
+
+    private fun getFacts() {
+        compositeDisposable.add(
+            Flowable.interval(0, 2, TimeUnit.SECONDS).flatMap {
+                getCatFactUseCase()
+                    .onErrorResumeNext {
+                        getLocalCatFactUseCase()
+                    }.toFlowable()
+                    .subscribeOn(Schedulers.io())
+
+            }.observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    _catsLiveData.value = Success(it)
+                }, {
                     _catsLiveData.value = Error(
-                        response.errorBody()?.string() ?: context.getString(
+                        it.message ?: context.getString(
                             R.string.default_error_text
                         )
                     )
-                }
-            }
-
-            override fun onFailure(call: Call<Fact>, t: Throwable) {
-                _catsLiveData.value = ServerError
-            }
-        })
+                })
+        )
     }
 
-    fun getFacts() {}
+    private var remoteCatFactCount = 0
+    private var localCatFactCount = 0
+
+    private fun getCatFactUseCase(): Single<Fact> {
+        return Single.just(Unit).flatMap {
+            remoteCatFactCount++
+            if (remoteCatFactCount % 4 == 0) {
+                throw Exception("getCatFactUseCase exception")
+            } else {
+                catsService.getCatFact()
+            }
+        }
+    }
+
+    private fun getLocalCatFactUseCase(): Single<Fact> {
+        return Single.just(Unit).flatMap {
+            localCatFactCount++
+            if (localCatFactCount % 2 == 0) {
+                throw Exception("getLocalCatFactUseCase exception")
+            } else {
+                localCatFactsGenerator.generateCatFact()
+            }
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.dispose()
+    }
 }
 
 class CatsViewModelFactory(
