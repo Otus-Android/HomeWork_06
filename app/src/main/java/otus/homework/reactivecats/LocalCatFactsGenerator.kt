7498269fd5c23803c639767d2a09ca5ab92b316b
vrazdyakonov <razdyakonov.viktor@gmail.com>
2024-04-10 20:47:50 +0300

    Implement LocalCatFactsGenerator.generateCatFactPeriodically

--- app/src/main/java/otus/homework/reactivecats/LocalCatFactsGenerator.kt
+++ app/src/main/java/otus/homework/reactivecats/LocalCatFactsGenerator.kt
@@ -1,12 +1,20 @@
 package otus.homework.reactivecats
 
 import android.content.Context
+import android.util.Log
+import io.reactivex.BackpressureStrategy
 import io.reactivex.Flowable
+import io.reactivex.FlowableOnSubscribe
+import io.reactivex.Scheduler
 import io.reactivex.Single
+import io.reactivex.schedulers.Schedulers
+import java.lang.Thread.sleep
+import java.util.concurrent.TimeUnit
 import kotlin.random.Random
 
+
 class LocalCatFactsGenerator(
-    private val context: Context
+    private val context : Context,
 ) {
 
     /**
@@ -14,7 +22,7 @@ class LocalCatFactsGenerator(
      * чтобы она возвращала Fact со случайной строкой  из массива строк R.array.local_cat_facts
      * обернутую в подходящий стрим(Flowable/Single/Observable и т.п)
      */
-    fun generateCatFact(): Single<Fact> {
+    fun generateCatFact() : Single<Fact> {
         return Single.fromCallable {
             val localFacts = context.resources.getStringArray(R.array.local_cat_facts)
             val randomFactIndex = Random.nextInt(0, localFacts.size)
@@ -27,8 +35,12 @@ class LocalCatFactsGenerator(
      * чтобы она эмитила Fact со случайной строкой из массива строк R.array.local_cat_facts каждые 2000 миллисекунд.
      * Если вновь заэмиченный Fact совпадает с предыдущим - пропускаем элемент.
      */
-    fun generateCatFactPeriodically(): Flowable<Fact> {
-        val success = Fact(context.resources.getStringArray(R.array.local_cat_facts)[Random.nextInt(5)])
-        return Flowable.empty()
+    fun generateCatFactPeriodically() : Flowable<Fact> {
+        return Flowable.interval(2000, TimeUnit.MILLISECONDS, Schedulers.io())
+            .map {
+                val facts = context.resources.getStringArray(R.array.local_cat_facts)
+                Fact(facts[Random.nextInt(5)])
+            }
+            .distinct()
     }
 }
