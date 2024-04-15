ff69401445aac8e74e5cd4763a23abd8709c7cdb
firkatdavletov <dr.firkat@ya.ru>
2024-04-15 14:05:42 +0500

    #1 Переведен сетевой запрос с `retrofit.Call` на RX цепочку. Подклбючение Retrofit адаптера, смена возвращаемого типа функций

--- app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
+++ app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
@@ -5,9 +5,13 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import retrofit2.Call
 import retrofit2.Callback
+import retrofit2.HttpException
 import retrofit2.Response
+import java.io.IOException
 
 class CatsViewModel(
     catsService: CatsService,
@@ -18,24 +22,30 @@ class CatsViewModel(
     private val _catsLiveData = MutableLiveData<Result>()
     val catsLiveData: LiveData<Result> = _catsLiveData
 
-    init {
-        catsService.getCatFact().enqueue(object : Callback<Fact> {
-            override fun onResponse(call: Call<Fact>, response: Response<Fact>) {
-                if (response.isSuccessful && response.body() != null) {
-                    _catsLiveData.value = Success(response.body()!!)
-                } else {
-                    _catsLiveData.value = Error(
-                        response.errorBody()?.string() ?: context.getString(
-                            R.string.default_error_text
-                        )
-                    )
-                }
-            }
+    private val compositeDisposable = CompositeDisposable()
 
-            override fun onFailure(call: Call<Fact>, t: Throwable) {
-                _catsLiveData.value = ServerError
-            }
-        })
+    init {
+        compositeDisposable.add(
+            catsService.getCatFact()
+                .subscribeOn(Schedulers.io())
+                .map<Result>{ fact -> Success(fact) }
+                .onErrorReturn { th ->
+                    return@onErrorReturn when(th) {
+                        is HttpException -> {
+                            Error(th.message())
+                        }
+                        is IOException -> {
+                            ServerError
+                        }
+                        else -> {
+                            Error(th.message ?: context.getString(R.string.default_error_text))
+                        }
+                    }
+                }
+                .subscribe {
+                    _catsLiveData.postValue(it)
+                }
+        )
     }
 
     fun getFacts() {}
