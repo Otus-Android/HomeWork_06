3c61ec5c517f9b89316692215a0b87558642ea80
bmv-2143 <bmv-2143@yandex.ru>
2024-07-10 21:58:25 +0300

    feat: add hw task 2
    Поменяйте логику в CatsViewModel.kt с колбеков на RX. Логику обработки
    успеха/ошибки из коллбека необходимо перенести в терминальные коллбеки
    RX цепочки. Не забудьте очистить подписки когда ViewModel уничтожается

--- app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
+++ app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
@@ -1,13 +1,14 @@
 package otus.homework.reactivecats
 
 import android.content.Context
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 
 class CatsViewModel(
     catsService: CatsService,
@@ -15,30 +16,43 @@ class CatsViewModel(
     context: Context
 ) : ViewModel() {
 
+    private val tag = javaClass.simpleName
     private val _catsLiveData = MutableLiveData<Result>()
     val catsLiveData: LiveData<Result> = _catsLiveData
+    private val subscriptionDisposables: CompositeDisposable = CompositeDisposable()
 
     init {
-        catsService.getCatFact().enqueue(object : Callback<Fact> {
-            override fun onResponse(call: Call<Fact>, response: Response<Fact>) {
-                if (response.isSuccessful && response.body() != null) {
-                    _catsLiveData.value = Success(response.body()!!)
-                } else {
-                    _catsLiveData.value = Error(
-                        response.errorBody()?.string() ?: context.getString(
-                            R.string.default_error_text
-                        )
-                    )
-                }
-            }
 
-            override fun onFailure(call: Call<Fact>, t: Throwable) {
-                _catsLiveData.value = ServerError
-            }
-        })
+        val catFactsSubscription = catsService
+            .getCatFact()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { fact ->
+                    _catsLiveData.value = Success(fact)
+                },
+                { error ->
+                    if (error is retrofit2.HttpException) {
+                        Log.e(tag, "ServerError: ${error.code()}")
+                        _catsLiveData.value = ServerError
+                    } else {
+                        Log.e(tag, "Error: $error")
+                        _catsLiveData.value = Error(
+                            error.message ?: context.getString(R.string.default_error_text)
+                        )
+                    }
+                }
+            )
+
+        subscriptionDisposables.add(catFactsSubscription)
     }
 
     fun getFacts() {}
+
+    override fun onCleared() {
+        super.onCleared()
+        subscriptionDisposables.dispose()
+    }
 }
 
 class CatsViewModelFactory(
