177ac174d28de714090e052d5db96f481c7dd98e
Roman Skaskevich <roman.skaskevich@gmail.com>
2024-07-13 00:22:22 +0300

    Implement generateCatFact

--- app/src/main/java/otus/homework/reactivecats/LocalCatFactsGenerator.kt
+++ app/src/main/java/otus/homework/reactivecats/LocalCatFactsGenerator.kt
@@ -15,7 +15,9 @@ class LocalCatFactsGenerator(
      * обернутую в подходящий стрим(Flowable/Single/Observable и т.п)
      */
     fun generateCatFact(): Single<Fact> {
-        return Single.never()
+        val facts = context.resources.getStringArray(R.array.local_cat_facts).toList()
+        val fact = facts[Random.nextInt(facts.size)]
+        return Single.just(Fact(fact))
     }
 
     /**
