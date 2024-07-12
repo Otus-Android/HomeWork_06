41d820aa249e0206bd2d9cffc6054bb973c35f67
Roman Skaskevich <roman.skaskevich@gmail.com>
2024-07-13 02:02:07 +0300

    Implement generateCatFactPeriodically

--- app/src/main/java/otus/homework/reactivecats/LocalCatFactsGenerator.kt
+++ app/src/main/java/otus/homework/reactivecats/LocalCatFactsGenerator.kt
@@ -3,11 +3,14 @@ package otus.homework.reactivecats
 import android.content.Context
 import io.reactivex.Flowable
 import io.reactivex.Single
+import java.util.concurrent.TimeUnit
 import kotlin.random.Random
 
 class LocalCatFactsGenerator(
     private val context: Context
 ) {
+
+    private val catFacts = context.resources.getStringArray(R.array.local_cat_facts)
 
     /**
      * Реализуйте функцию otus.homework.reactivecats.LocalCatFactsGenerator#generateCatFact так,
@@ -15,8 +18,7 @@ class LocalCatFactsGenerator(
      * обернутую в подходящий стрим(Flowable/Single/Observable и т.п)
      */
     fun generateCatFact(): Single<Fact> {
-        val facts = context.resources.getStringArray(R.array.local_cat_facts).toList()
-        val fact = facts[Random.nextInt(facts.size)]
+        val fact = catFacts[Random.nextInt(catFacts.size - 1)]
         return Single.just(Fact(fact))
     }
 
@@ -26,7 +28,9 @@ class LocalCatFactsGenerator(
      * Если вновь заэмиченный Fact совпадает с предыдущим - пропускаем элемент.
      */
     fun generateCatFactPeriodically(): Flowable<Fact> {
-        val success = Fact(context.resources.getStringArray(R.array.local_cat_facts)[Random.nextInt(5)])
-        return Flowable.empty()
+        return Flowable.interval(2, TimeUnit.SECONDS)
+            .map { Random.nextInt(catFacts.size - 1) }
+            .distinctUntilChanged()
+            .map { Fact(catFacts[it]) }
     }
 }
