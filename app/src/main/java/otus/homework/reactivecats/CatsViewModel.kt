d952c2a5248c59179d25f8823c6b24dcff0c3405
Ivan Butrimov <ivanbutrimov@gmail.com>
2024-07-15 23:56:30 +0300

    [otus] rx home work

--- app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
+++ app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
@@ -5,40 +5,105 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import io.reactivex.Flowable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 class CatsViewModel(
-    catsService: CatsService,
-    localCatFactsGenerator: LocalCatFactsGenerator,
-    context: Context
+    private val catsService: CatsService,
+    val localCatFactsGenerator: LocalCatFactsGenerator,
+    val context: Context,
 ) : ViewModel() {
 
     private val _catsLiveData = MutableLiveData<Result>()
     val catsLiveData: LiveData<Result> = _catsLiveData
 
-    init {
-        catsService.getCatFact().enqueue(object : Callback<Fact> {
-            override fun onResponse(call: Call<Fact>, response: Response<Fact>) {
-                if (response.isSuccessful && response.body() != null) {
-                    _catsLiveData.value = Success(response.body()!!)
-                } else {
-                    _catsLiveData.value = Error(
-                        response.errorBody()?.string() ?: context.getString(
-                            R.string.default_error_text
-                        )
-                    )
-                }
-            }
+    private val compositeDisposable = CompositeDisposable()
 
-            override fun onFailure(call: Call<Fact>, t: Throwable) {
-                _catsLiveData.value = ServerError
-            }
-        })
+    init {
+//        getCatFact()
+//        generateRandomCatFacts()
+//        generateCatFactPeriodically()
+        getFacts()
     }
 
-    fun getFacts() {}
+    private fun getCatFact() {
+        val disposable = catsService.getCatFact()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { response ->
+                    setResult(response)
+                },
+                { t ->
+                    _catsLiveData.value = ServerError
+                }
+            )
+        compositeDisposable.add(disposable)
+    }
+
+    private fun generateRandomCatFacts() {
+        val disposable = localCatFactsGenerator.generateCatFact()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { response ->
+                    setResult(response)
+                },
+                { t ->
+                    _catsLiveData.value = ServerError
+                }
+            )
+
+        compositeDisposable.add(disposable)
+    }
+
+    private fun generateCatFactPeriodically() {
+        val disposable = localCatFactsGenerator.generateCatFactPeriodically()
+            .subscribeOn(Schedulers.io())
+            .observeOn(Schedulers.io())
+            .subscribe(
+                { response ->
+                    setResult(response)
+                },
+                { t ->
+                    _catsLiveData.value = ServerError
+                }
+            )
+        compositeDisposable.add(disposable)
+    }
+
+    private fun getFacts() {
+        val disposable = Flowable.interval(2000, TimeUnit.MILLISECONDS)
+            .flatMapSingle { catsService.getCatFact() }
+            .onErrorResumeNext(localCatFactsGenerator.generateCatFactPeriodically())
+            .subscribe(
+                { response ->
+                    setResult(response)
+                },
+                { t ->
+                    _catsLiveData.value = ServerError
+                }
+            )
+        compositeDisposable.add(disposable)
+    }
+
+    private fun setResult(fact: Fact?) {
+        if (fact != null) {
+            _catsLiveData.value = Success(fact)
+        } else {
+            _catsLiveData.value = Error(
+                context.getString(R.string.default_error_text)
+            )
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.clear()
+    }
 }
 
 class CatsViewModelFactory(
