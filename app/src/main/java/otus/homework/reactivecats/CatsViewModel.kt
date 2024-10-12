46ca51b71dacc1746743e8b3b77d1d3e99da852c
GeorgeBozon <georgebozon@gmail.com>
2024-10-12 13:47:33 +0300

    1. Подключил адаптер для ретрофита 2. Поменял колбеки на rxJava обсервер

--- app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
+++ app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
@@ -5,9 +5,9 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 
 class CatsViewModel(
     catsService: CatsService,
@@ -18,27 +18,31 @@ class CatsViewModel(
     private val _catsLiveData = MutableLiveData<Result>()
     val catsLiveData: LiveData<Result> = _catsLiveData
 
-    init {
-        catsService.getCatFact().enqueue(object : Callback<Fact> {
-            override fun onResponse(call: Call<Fact>, response: Response<Fact>) {
-                if (response.isSuccessful && response.body() != null) {
-                    _catsLiveData.value = Success(response.body()!!)
-                } else {
-                    _catsLiveData.value = Error(
-                        response.errorBody()?.string() ?: context.getString(
-                            R.string.default_error_text
-                        )
-                    )
-                }
-            }
+    private var disposable: Disposable
 
-            override fun onFailure(call: Call<Fact>, t: Throwable) {
-                _catsLiveData.value = ServerError
-            }
-        })
+    init {
+        disposable = catsService.getCatFact()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { fact ->
+                    _catsLiveData.value = Success(fact)
+
+                }, { error ->
+                    _catsLiveData.value = Error(error.message?: context.getString(
+                        R.string.default_error_text
+                    ))
+                })
+
     }
 
-    fun getFacts() {}
+    override fun onCleared() {
+        disposable.dispose()
+    }
+
+    fun getFacts() {
+
+    }
 }
 
 class CatsViewModelFactory(
@@ -55,4 +59,3 @@ class CatsViewModelFactory(
 sealed class Result
 data class Success(val fact: Fact) : Result()
 data class Error(val message: String) : Result()
-object ServerError : Result()
--- app/src/main/java/otus/homework/reactivecats/MainActivity.kt
+++ app/src/main/java/otus/homework/reactivecats/MainActivity.kt
@@ -1,11 +1,9 @@
 package otus.homework.reactivecats
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.viewModels
-import androidx.lifecycle.lifecycleScope
-import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,7 +24,6 @@ class MainActivity : AppCompatActivity() {
             when (result) {
                 is Success -> view.populate(result.fact)
                 is Error -> Toast.makeText(this, result.message, Toast.LENGTH_LONG).show()
-                ServerError -> Snackbar.make(view, "Network error", 1000).show()
             }
         }
     }
