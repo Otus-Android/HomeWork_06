3b25a2465143118edf950dded301ce1b7113f298
GeorgeBozon <georgebozon@gmail.com>
2024-10-12 15:05:39 +0300

    3. Реализовал функцию LocalCatFactsGenerator.generateCatFact 4. Реализовал функцию LocalCatFactsGenerator.generateCatFactPeriodically

--- app/src/main/java/otus/homework/reactivecats/LocalCatFactsGenerator.kt
+++ app/src/main/java/otus/homework/reactivecats/LocalCatFactsGenerator.kt
@@ -3,6 +3,7 @@ package otus.homework.reactivecats
 import android.content.Context
 import io.reactivex.Flowable
 import io.reactivex.Single
+import java.util.concurrent.TimeUnit
 import kotlin.random.Random
 
 class LocalCatFactsGenerator(
@@ -14,17 +15,22 @@ class LocalCatFactsGenerator(
      * чтобы она возвращала Fact со случайной строкой  из массива строк R.array.local_cat_facts
      * обернутую в подходящий стрим(Flowable/Single/Observable и т.п)
      */
-    fun generateCatFact(): Single<Fact> {
-        return Single.never()
-    }
+    fun generateCatFact(): Single<Fact> = Single.create { emitter ->
+            val stringArray = context.resources.getStringArray(R.array.local_cat_facts)
+            emitter.onSuccess(
+                Fact(stringArray[Random.nextInt(stringArray.lastIndex)])
+            )
+        }
+
 
     /**
      * Реализуйте функцию otus.homework.reactivecats.LocalCatFactsGenerator#generateCatFactPeriodically так,
      * чтобы она эмитила Fact со случайной строкой из массива строк R.array.local_cat_facts каждые 2000 миллисекунд.
      * Если вновь заэмиченный Fact совпадает с предыдущим - пропускаем элемент.
      */
-    fun generateCatFactPeriodically(): Flowable<Fact> {
-        val success = Fact(context.resources.getStringArray(R.array.local_cat_facts)[Random.nextInt(5)])
-        return Flowable.empty()
+    fun generateCatFactPeriodically(): Flowable<Fact> =
+        Flowable.interval(2, TimeUnit.SECONDS).map {
+            val stringArray = context.resources.getStringArray(R.array.local_cat_facts)
+            Fact(stringArray[Random.nextInt(stringArray.lastIndex)])
+        }.distinctUntilChanged()
     }
-}
