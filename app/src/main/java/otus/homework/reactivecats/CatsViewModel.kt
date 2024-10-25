95461be0a8fb2f693b8dd6cf55d6eba3be43824c
Werechess <werechess@gmail.com>
2024-10-26 00:18:35 +0300

    feat: homework01

--- app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
+++ app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
@@ -5,40 +5,55 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import java.net.SocketException
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
+import java.util.concurrent.TimeUnit
 
 class CatsViewModel(
-    catsService: CatsService,
-    localCatFactsGenerator: LocalCatFactsGenerator,
+    private val catsService: CatsService,
+    private val localCatFactsGenerator: LocalCatFactsGenerator,
     context: Context
 ) : ViewModel() {
 
     private val _catsLiveData = MutableLiveData<Result>()
     val catsLiveData: LiveData<Result> = _catsLiveData
 
-    init {
-        catsService.getCatFact().enqueue(object : Callback<Fact> {
-            override fun onResponse(call: Call<Fact>, response: Response<Fact>) {
-                if (response.isSuccessful && response.body() != null) {
-                    _catsLiveData.value = Success(response.body()!!)
-                } else {
-                    _catsLiveData.value = Error(
-                        response.errorBody()?.string() ?: context.getString(
-                            R.string.default_error_text
-                        )
-                    )
-                }
-            }
+    private lateinit var disposable: Disposable
+    private val defaultError = context.getString(R.string.default_error_text)
 
-            override fun onFailure(call: Call<Fact>, t: Throwable) {
-                _catsLiveData.value = ServerError
-            }
-        })
+    init {
+        getFacts()
     }
 
-    fun getFacts() {}
+    override fun onCleared() {
+        super.onCleared()
+        disposable.dispose()
+    }
+
+    private fun getFacts() {
+        disposable = catsService.getCatFact()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .onErrorResumeNext { localCatFactsGenerator.generateCatFact() }
+            .repeatWhen { it.delay(2, TimeUnit.SECONDS) }
+            .subscribe(
+                { fact -> _catsLiveData.value = Success(fact) },
+                { throwable ->
+                    if (isNetworkTypeException(throwable)) _catsLiveData.value = NetworkError
+                    else _catsLiveData.value = Error(throwable.message ?: defaultError)
+                }
+            )
+    }
+
+    private fun <T> isNetworkTypeException(t: T): Boolean {
+        return t is SocketException ||
+                t is SocketTimeoutException ||
+                t is UnknownHostException
+    }
 }
 
 class CatsViewModelFactory(
@@ -55,4 +70,4 @@ class CatsViewModelFactory(
 sealed class Result
 data class Success(val fact: Fact) : Result()
 data class Error(val message: String) : Result()
-object ServerError : Result()
+object NetworkError : Result()
--- app/src/main/java/otus/homework/reactivecats/MainActivity.kt
+++ app/src/main/java/otus/homework/reactivecats/MainActivity.kt
@@ -1,10 +1,9 @@
 package otus.homework.reactivecats
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.viewModels
-import androidx.lifecycle.lifecycleScope
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
@@ -26,7 +25,7 @@ class MainActivity : AppCompatActivity() {
             when (result) {
                 is Success -> view.populate(result.fact)
                 is Error -> Toast.makeText(this, result.message, Toast.LENGTH_LONG).show()
-                ServerError -> Snackbar.make(view, "Network error", 1000).show()
+                NetworkError -> Snackbar.make(view, "Network error", 1000).show()
             }
         }
     }
