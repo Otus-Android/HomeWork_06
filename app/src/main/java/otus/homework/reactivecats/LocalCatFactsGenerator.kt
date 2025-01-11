f31f2d8e6ce6b91964a6783297195a52982341d6
Toha8 <a.harbatovich@gmail.com>
2025-01-11 13:59:00 +0300

    Task 3

--- app/src/main/java/otus/homework/reactivecats/LocalCatFactsGenerator.kt
+++ app/src/main/java/otus/homework/reactivecats/LocalCatFactsGenerator.kt
@@ -8,14 +8,14 @@ import kotlin.random.Random
 class LocalCatFactsGenerator(
     private val context: Context
 ) {
-
+    private val catFacts = context.resources.getStringArray(R.array.local_cat_facts)
     /**
      * Реализуйте функцию otus.homework.reactivecats.LocalCatFactsGenerator#generateCatFact так,
      * чтобы она возвращала Fact со случайной строкой  из массива строк R.array.local_cat_facts
      * обернутую в подходящий стрим(Flowable/Single/Observable и т.п)
      */
     fun generateCatFact(): Single<Fact> {
-        return Single.never()
+        return Single.fromCallable { Fact(catFacts[Random.nextInt(5)]) }
     }
 
     /**
