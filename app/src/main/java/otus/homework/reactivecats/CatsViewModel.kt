5e0db1417993d56e23cde353b81c3d1e7da85716
ivanv <ivdovin@mail.ru>
2025-04-13 20:16:30 +0300

    Implemented 'RxJavaCallAdapter' and rxjava subscription instead of retrofit call. Implemented logic of 'LocalCatFacsGenerator'.

--- app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
+++ app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
@@ -5,40 +5,66 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import retrofit2.HttpException
+import java.util.concurrent.TimeUnit
 
 class CatsViewModel(
-    catsService: CatsService,
-    localCatFactsGenerator: LocalCatFactsGenerator,
+    private val catsService: CatsService,
+    private val localCatFactsGenerator: LocalCatFactsGenerator,
     context: Context
 ) : ViewModel() {
 
+    private val _subscriptions = CompositeDisposable()
     private val _catsLiveData = MutableLiveData<Result>()
     val catsLiveData: LiveData<Result> = _catsLiveData
 
     init {
-        catsService.getCatFact().enqueue(object : Callback<Fact> {
-            override fun onResponse(call: Call<Fact>, response: Response<Fact>) {
-                if (response.isSuccessful && response.body() != null) {
-                    _catsLiveData.value = Success(response.body()!!)
-                } else {
-                    _catsLiveData.value = Error(
-                        response.errorBody()?.string() ?: context.getString(
-                            R.string.default_error_text
-                        )
-                    )
+        catsService.getCatFact()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { fact -> emitResult(Success(fact)) },
+                { error ->
+                    when (error is HttpException) {
+                        true -> emitResult(handleHttpError(error, context))
+                        else -> emitResult(ServerError)
+                    }
                 }
-            }
-
-            override fun onFailure(call: Call<Fact>, t: Throwable) {
-                _catsLiveData.value = ServerError
-            }
-        })
+            ).addToSubscriptions()
     }
 
-    fun getFacts() {}
+    private fun emitResult(result: Result) {
+        _catsLiveData.postValue(result)
+    }
+
+    private fun handleHttpError(error: HttpException, context: Context): Result {
+        val errorBody = error.response()?.errorBody()?.string()
+        return Error(errorBody ?: context.getString(R.string.default_error_text))
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        _subscriptions.clear()
+    }
+
+    fun getFacts() {
+        catsService.getCatFact()
+            .subscribeOn(Schedulers.io())
+            .onErrorResumeNext { localCatFactsGenerator.generateCatFact() }
+            .delay(2, TimeUnit.SECONDS)
+            .retry(3)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { fact -> emitResult(Success(fact)) },
+                { emitResult(ServerError) }
+            ).addToSubscriptions()
+    }
+
+    private fun Disposable.addToSubscriptions() = _subscriptions.add(this)
 }
 
 class CatsViewModelFactory(
