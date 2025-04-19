e01937dd792fe9355975c1632d4c4b26db922bf2
SpiritualAdviser <pomaxam@narod.ru>
2025-04-19 10:56:20 +0300

    network request from retrofit.Call to the RX chain

--- app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
+++ app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
@@ -1,45 +1,60 @@
 package otus.homework.reactivecats
 
+import android.annotation.SuppressLint
 import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import retrofit2.HttpException
 
+
+@SuppressLint("CheckResult")
 class CatsViewModel(
-    catsService: CatsService,
+    private val catsService: CatsService,
     localCatFactsGenerator: LocalCatFactsGenerator,
     context: Context
 ) : ViewModel() {
 
     private val _catsLiveData = MutableLiveData<Result>()
     val catsLiveData: LiveData<Result> = _catsLiveData
+    private val compositeDisposable = CompositeDisposable()
 
     init {
-        catsService.getCatFact().enqueue(object : Callback<Fact> {
-            override fun onResponse(call: Call<Fact>, response: Response<Fact>) {
-                if (response.isSuccessful && response.body() != null) {
-                    _catsLiveData.value = Success(response.body()!!)
-                } else {
-                    _catsLiveData.value = Error(
-                        response.errorBody()?.string() ?: context.getString(
-                            R.string.default_error_text
-                        )
-                    )
-                }
-            }
+        compositeDisposable.add(
+            catsService.getCatFact()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                    { fact -> onCatsServiceSuccess(fact) },
+                    { error -> onCatsServiceError(error) }
+                )
+        )
+    }
 
-            override fun onFailure(call: Call<Fact>, t: Throwable) {
-                _catsLiveData.value = ServerError
-            }
-        })
+    private fun onCatsServiceSuccess(fact: Fact) {
+        _catsLiveData.value = Success(fact)
+    }
+
+    private fun onCatsServiceError(error: Throwable) {
+        _catsLiveData.value = if (error is HttpException) {
+            ServerError
+        } else {
+            Error((error.message ?: R.string.default_error_text).toString())
+        }
     }
 
     fun getFacts() {}
+
+    override fun onCleared() {
+        compositeDisposable.clear()
+        super.onCleared()
+    }
 }
+
 
 class CatsViewModelFactory(
     private val catsRepository: CatsService,
