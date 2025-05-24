1636db5fd8cce1a9ac129bbdfca03a4e033e8c09
Bogdan Patseev <patseev1987@gmail.com>
2025-05-25 00:17:49 +0300

    add rx adapter

--- app/src/main/java/otus/homework/reactivecats/DiContainer.kt
+++ app/src/main/java/otus/homework/reactivecats/DiContainer.kt
@@ -1,7 +1,10 @@
 package otus.homework.reactivecats
 
 import android.content.Context
+import io.reactivex.internal.schedulers.RxThreadFactory
+import io.reactivex.plugins.RxJavaPlugins
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 class DiContainer {
@@ -9,6 +12,7 @@ class DiContainer {
     private val retrofit by lazy {
         Retrofit.Builder()
             .baseUrl("https://cat-fact.herokuapp.com/facts/")
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
