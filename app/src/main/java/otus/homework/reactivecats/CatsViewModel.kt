c83d7f4ee7247a2a45971c77770a524ad4062985
Amir4ik <amir4ik11@gmail.com>
2025-07-02 23:37:26 +0400

    2) Changed Logic in ViewModel

--- app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
+++ app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
@@ -5,9 +5,11 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import retrofit2.HttpException
+import java.io.IOException
 
 class CatsViewModel(
     catsService: CatsService,
@@ -17,30 +19,45 @@ class CatsViewModel(
 
     private val _catsLiveData = MutableLiveData<Result>()
     val catsLiveData: LiveData<Result> = _catsLiveData
+    private val observable = CompositeDisposable()
 
     init {
-        catsService.getCatFact().enqueue(object : Callback<Fact> {
-            override fun onResponse(call: Call<Fact>, response: Response<Fact>) {
-                if (response.isSuccessful && response.body() != null) {
-                    _catsLiveData.value = Success(response.body()!!)
-                } else {
-                    _catsLiveData.value = Error(
-                        response.errorBody()?.string() ?: context.getString(
-                            R.string.default_error_text
-                        )
-                    )
-                }
-            }
+        observable.add(
+            catsService.getCatFact()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                    { fact ->
+                        _catsLiveData.value = Success(fact)
+                    },
+                    { error ->
+                        _catsLiveData.value = when (error) {
+                            is HttpException -> {
+                                val errorMessage = error.response()?.message()
+                                    ?: context.getString(R.string.default_error_text)
+                                Error(errorMessage)
+                            }
 
-            override fun onFailure(call: Call<Fact>, t: Throwable) {
-                _catsLiveData.value = ServerError
-            }
-        })
+                            is IOException -> ServerError
+                            else ->
+                                Error(
+                                    error.message ?: context.getString(R.string.default_error_text)
+                                )
+                        }
+                    }
+                )
+        )
     }
 
     fun getFacts() {}
+
+    override fun onCleared() {
+        super.onCleared()
+        observable.clear()
+    }
 }
 
+@Suppress("UNCHECKED_CAST")
 class CatsViewModelFactory(
     private val catsRepository: CatsService,
     private val localCatFactsGenerator: LocalCatFactsGenerator,
@@ -55,4 +72,5 @@ class CatsViewModelFactory(
 sealed class Result
 data class Success(val fact: Fact) : Result()
 data class Error(val message: String) : Result()
-object ServerError : Result()
+data object Loading : Result()
+data object ServerError : Result()
