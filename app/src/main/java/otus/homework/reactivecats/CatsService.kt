24e4ff4e50ba25a4b5e06772e2803f9482e81415
Maxim Kachinkin <m.kachinkin@dodobrands.io>
2025-09-07 17:35:55 +0300

    Update project

--- app/src/main/java/otus/homework/reactivecats/CatsService.kt
+++ app/src/main/java/otus/homework/reactivecats/CatsService.kt
@@ -5,6 +5,7 @@ import retrofit2.http.GET
 
 interface CatsService {
 
-    @GET("random?animal_type=cat")
+    //@GET("random?animal_type=cat")
+    @GET("fact")
     fun getCatFact(): Call<Fact>
 }
